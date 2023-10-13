efb31dfffc145863881769636a20c0f84812a2d5
Abdullah Islam <abdullahislam9191@gmail.com>
2023-10-13 16:24:49 +0500

    booo

--- src/components/Imperial-weight.js
+++ src/components/Imperial-weight.js
@@ -1,11 +1,22 @@
 import '../App.css';
+import { useContext } from 'react';
+import { weightContext } from '../App';
+
 
 function ImperialWeight() {
+    const {activeWeightButton, setActiveWeightButton, metricWeightValue, setMetricWeightValue, imperialWeightValue, setImperialWeightValue} = useContext(weightContext);
+
+    const changeHandler = (e) => {
+        setImperialWeightValue(e.target.value);
+        setMetricWeightValue('')
+    }
     return (
-        <input 
+        <input
+         value={imperialWeightValue}
+         onChange={changeHandler}
         type='text'
         inputMode="numeric"
-        placeHolder="Pounds"
+        placeholder="Pounds"
         id="imperial-weight-input"            
     />
     )
--- src/components/Metric-weight.js
+++ src/components/Metric-weight.js
@@ -1,11 +1,22 @@
 import '../App.css';
+import { weightContext } from '../App';
+import { useContext } from 'react';
 
 function MetricWeight() {
+    const {activeWeightButton, setActiveWeightButton, metricWeightValue, setMetricWeightValue, imperialWeightValue, setImperialWeightValue} = useContext(weightContext);
+    
+    const changeHandler = (e) => {
+        setMetricWeightValue(e.target.value)
+        setImperialWeightValue('')
+        
+    }
     return(
         <input 
+        value={metricWeightValue}
+        onChange={changeHandler}
         type='text'
         inputMode="numeric"
-        placeHolder="Kilograms"
+        placeholder="Kilograms"
         id="metric-input"            
     />
     )
